0fd34e999739065012d9a586fea43d2a04341539
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-04-24 19:42:16 -0400

    Minimap Updates
    Yes it does. Certainly is.
    
    Fuck dude, it sure is. yeet yeet. Push me d4ddy

---
+++ SideScroller/Assets/Scripts/Main/IconFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconFollow : MonoBehaviour
+{
+	public GameObject podToFollow;
+	void Update () 
+	{
+		this.transform.position = podToFollow.transform.position + new Vector3(0, 0, -10);
+	}
+}
--- SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
+++ SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,42 +13,57 @@ public class Minimapbehaviour : MonoBehaviour
     private int minimapMode = 0; //0 = small bottom
                                  //1 = big middle
                                  //2 = None
-    
-    
+    private Vector4 minimapCamStart;
+    private Rect minimapStart;
+    private void Start()
+    {
+
+        minimapCamStart = new Vector4(minimapCam.rect.x, minimapCam.rect.y, minimapCam.rect.width, minimapCam.rect.height);
+        minimapStart = new Rect(minimapCamStart.w, minimapCamStart.x, minimapCamStart.y, minimapCamStart.z);
+    }
+
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
+            
             if (minimapMode == 0)
             {
                 minimapMode = 1;
+                print(minimapMode);
             }
-            if (minimapMode == 1)
+            else if (minimapMode == 1)
             {
                 minimapMode = 2;
+                print(minimapMode);
             }
-            if (minimapMode == 2)
+            else if (minimapMode == 2)
             {
                 minimapMode = 0;
-            }
-            
-            switch (minimapMode)
-            {
-                case 0:
-                    minimapCam.orthographicSize = 80;
-                    break;
-                
-                
-                case 1:
-                    minimapCam.orthographicSize = 250;
-                    minimapCam.rect = Camera.main.rect;
-                    break;
-                
-                
-                case 2:
-                    minimapCam.enabled = false;
-                    break;
+                print(minimapMode);
             }
         }
+
+        switch (minimapMode)
+        {
+            case 0:
+                minimapCam.ResetAspect();
+                minimapCam.orthographicSize = 100;
+                minimapCam.enabled = true;
+                break;
+                
+                
+            case 1:
+                minimapCam.ResetAspect();
+                minimapCam.enabled = true;
+                break;
+                
+                
+            case 2:
+                minimapCam.enabled = false;
+                break;
+        }
+
     }
 }
