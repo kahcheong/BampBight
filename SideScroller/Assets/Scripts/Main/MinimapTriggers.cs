974b2cb52b70a21bca4f8ea0082376a6b3bfacae
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-04-24 17:57:32 -0400

    Minimap Appear Good
    It do good yeet yeet wurk it gurl

---
+++ SideScroller/Assets/Scripts/Main/MinimapTriggers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapTriggers : MonoBehaviour 
+{
+
+	private bool beenTriggered = false;
+    
+	[SerializeField]
+	public Image flipImage;
+    
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player" && !beenTriggered)
+		{
+			beenTriggered = !beenTriggered;
+			flipImage.enabled = false;
+		}
+	}
+}
--- SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
+++ SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
@@ -6,15 +6,6 @@ using UnityEngine.UI;
 
 public class Minimapbehaviour : MonoBehaviour
 {
-    public Canvas minimapCanas;
-    public Image hideLayer1;
-    public Image hideLayer2;
-    public Image hideLayer3;
-    public Image hideLayer4;
-    public Image hideLayer5;
-    public Image hideLayer6;
-    public Image hideLayer7;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
