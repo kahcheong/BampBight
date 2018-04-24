ce52b8bfa5a570c59fa6906f882ab1aa73271f9f
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-04-24 18:09:49 -0400

    Okay

--- SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
+++ SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
@@ -1,16 +1,53 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Minimapbehaviour : MonoBehaviour
 {
+    public Camera minimapCam;
+    private int minimapMode = 0; //0 = small bottom
+                                 //1 = big middle
+                                 //2 = None
+    
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
+            if (minimapMode == 0)
+            {
+                minimapMode = 1;
+            }
+            if (minimapMode == 1)
+            {
+                minimapMode = 2;
+            }
+            if (minimapMode == 2)
+            {
+                minimapMode = 0;
+            }
             
+            switch (minimapMode)
+            {
+                case 0:
+                    minimapCam.orthographicSize = 80;
+                    break;
+                
+                
+                case 1:
+                    minimapCam.orthographicSize = 250;
+                    minimapCam.rect = Camera.main.rect;
+                    break;
+                
+                
+                case 2:
+                    minimapCam.enabled = false;
+                    break;
+            }
         }
     }
 }
