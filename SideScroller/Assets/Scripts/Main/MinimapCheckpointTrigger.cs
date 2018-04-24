bb6f5f4a09ee01775aa339ae4a2ccc01b00da4a2
ManPumpkin <32469672+ManPumpkin@users.noreply.github.com>
2018-04-24 17:36:40 -0400

    Minimap Things
    yeet

---
+++ SideScroller/Assets/Scripts/Main/MinimapCheckpointTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class MinimapCheckpointTrigger : MonoBehaviour
+{
+    private bool beenTriggered = false;
+    public Image flipImage;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !beenTriggered)
+        {
+            flipImage.enabled = false;
+        }
+    }
+}
---
+++ SideScroller/Assets/Scripts/Main/Minimapbehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimapbehaviour : MonoBehaviour
+{
+    public Canvas minimapCanas;
+    public Image hideLayer1;
+    public Image hideLayer2;
+    public Image hideLayer3;
+    public Image hideLayer4;
+    public Image hideLayer5;
+    public Image hideLayer6;
+    public Image hideLayer7;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            
+        }
+    }
+}
